c6e2689d5d18c5b170a89a6f50963d5ecd6a8917
hlint <hanlintang813@gmail.com>
2026-01-12 23:33:25 -0800

    docker image pushing scripts

---
+++ scripts/push_docker_dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://docs.docker.com/build/building/multi-platform/
+# docker buildx build --platform=linux/amd64 --push -t hlint/knowlink:dev .
+docker buildx build --platform=linux/amd64,linux/arm64 --push -t hlint/knowlink:dev .
---
+++ scripts/push_docker_main.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# https://docs.docker.com/build/building/multi-platform/
+docker buildx build --platform=linux/amd64,linux/arm64 --push -t hlint/knowlink:latest .
